bed0b01d07933d5b3783124970c13d63ed61f4e0
Gabriela Rodrigues <gabriela.matias@sou.inteli.edu.br>
2022-11-09 20:18:06 -0300

    update ex1/sem1

--- SEMANA_01/EXERCICIOS/EX1/DEV/main.cpp
+++ SEMANA_01/EXERCICIOS/EX1/DEV/main.cpp
@@ -11,27 +11,25 @@
 
 using namespace std;
 
-int ajuste(){
-  float medida;
-  float val_min;
-  float val_max;
-  
-  cout << "Indique um valor" << endl;
-  cin >> medida; 
-
-  cout << "Indique um valor minimo:" << endl;
-  cin >> val_min;
-
-  cout << "Indique um valor maximo:" << endl;
-  cin >> val_max;
-  
+int ajuste(float medida, float val_min, float val_max){
   float resultado; //Indica a porcentagem em que o valor medida esta posicionado entre o minimo e o maximo.
 
   resultado = (medida - val_min) / (val_max - val_min);
-  cout << resultado << endl;
-  return 0;
+  return resultado;
 };
 
+/*
+// Descomente a função main abaixo para testar o exercício 1
+int main(){
+  cout << "Teste Ex 1" << endl;
+        cout << ajuste(100,100,400) << endl;
+  //valor esperado: 0
+        cout << ajuste(400,100,400) << endl;
+  //valor esperado: 1
+        cout << ajuste(250,100,400) << endl;
+  //valor esperado: 0.5
+}
+*/
 
 // 2 - Faça uma função que simule a leitura de um sensor lendo o 
 // valor do teclado ao final a função retorna e ste valor
@@ -40,9 +38,17 @@ float leitura(){
   float valor;
   cout << "Entrada do Sensor:" << endl;
   cin >> valor;
-  cout << valor << endl;
-  return 0;
+  return valor;
 }
+
+/*
+// Descomente a função main abaixo para testar o exercício 2
+int main(){
+      float cmd = leitura();
+      cout << "Comando Recebido: " << cmd << endl;
+}
+*/
+
 
 // 3 - Faça uma função que armazena uma medida inteira qualquer 
 // em um vetor fornecido. Note que como C não possui vetores 
@@ -51,29 +57,58 @@ float leitura(){
 // Evite também que, por acidente, um valor seja escrito em 
 // uma área de memória fora do vetor
 
-int vetores(int val_max, int val_final, int vetor[]){
-  int prox_valor;
-  int medida;
-
-  cout << "Indique um valor:" << endl;
-	cin >> medida;
-  
-	prox_valor = val_final + 1;
-
-	if((medida) > val_max){
-	cout << "Erro:" << endl;
-    cout << "Valor Maximo:" << endl;
-    cout << val_max << endl;
-    cout << "Ultima Posição:" << endl;
-    cout << val_final << endl;
-    
-	} else {
-		vetor[prox_valor] = medida;
-	}
-
-  cout << vetor << endl;
-	return 0;
+void insereVetor(int elemento, int valorMax, int lastPos, int Vetor[]){
+  if(valorMax <= indice){
+    cout << "Erro:" << valorMax << "indice:" << indice << endl;
+  }
+  else{
+    Vetor[indice] = elemento;
+    indice++;
+    cout << "indice:" << indice << endl;
+  }
 }
+
+/*
+int main(){
+  int valorMax = 12;
+  int lastPos = -1;
+  int Numbers[valorMax];
+  insereVetor(9, valorMax, lastPos, Numbers);
+  lastPos = lastPos + 1;
+  insereVetor(8, valorMax, lastPos, Numbers);
+  lastPos = lastPos + 1;
+  insereVetor(7, valorMax, lastPos, Numbers);
+  lastPos = lastPos + 1;
+  insereVetor(6, valorMax, lastPos, Numbers);
+  lastPos = lastPos + 1;
+  insereVetor(5, valorMax, lastPos, Numbers);
+  lastPos = lastPos + 1;
+  insereVetor(4, valorMax, lastPos, Numbers);
+  lastPos = lastPos + 1;
+  insereVetor(3, valorMax, lastPos, Numbers);
+  lastPos = lastPos + 1;
+  insereVetor(2, valorMax, lastPos, Numbers);
+  lastPos = lastPos + 1;
+  insereVetor(1, valorMax, lastPos, Numbers);
+  lastPos = lastPos + 1;
+  insereVetor(0, valorMax, lastPos, Numbers);
+  lastPos = lastPos + 1;
+  insereVetor(100, valorMax, lastPos, Numbers);
+  // valor esperado: mensagem de erro "Erro: valorMax: 10, lastPos + 1 (indice):10"
+  
+  cout << Numbers[0] << endl; // valor esperado: 9
+  cout << Numbers[1] << endl; // valor esperado: 8
+  cout << Numbers[2] << endl; // valor esperado: 7
+  cout << Numbers[3] << endl; // valor esperado: 6
+  cout << Numbers[4] << endl; // valor esperado: 5
+  cout << Numbers[5] << endl; // valor esperado: 4
+  cout << Numbers[6] << endl; // valor esperado: 3
+  cout << Numbers[7] << endl; // valor esperado: 2
+  cout << Numbers[8] << endl; // valor esperado: 1
+  cout << Numbers[9] << endl; // valor esperado: 0
+}
+*/
+
 
 // 4 - Faça uma função que recebe um vetor com 4 posições que contém 
 // o valor da distância de um pequeno robô até cada um dos seus 4 lados.
@@ -126,8 +161,40 @@ int maiorDist(int Vetor[]){
   sort(Vetor, Vetor+4);
   
   cout << "O Maior Valor é" << endl;
-  cout << Vetor[3] << endl;
+  return Vetor[3];
 }
+
+/*
+// Descomente a função main abaixo para testar o exercício 3
+int main(){
+//Considere que os valores sempre serão distintos
+//Considere valores de distância inteiros entre 0 e 100
+// "Direita", "Esquerda", "Frente", "Tras"
+int posicoes [4] = {0, 20, 100, 50};
+cout << dirMaiorDist(posicoes) << endl;
+//valor esperado: Frente
+cout << maiorDist(posicoes) << endl;
+//valor esperado: 100
+// "Direita", "Esquerda", "Frente", "Tras"
+int posicoes2 [4] = {95, 70, 80, 50};
+cout << dirMaiorDist(posicoes2) << endl;
+//valor esperado: Direita
+cout << maiorDist(posicoes2) << endl;
+//valor esperado: 95
+// "Direita", "Esquerda", "Frente", "Tras"
+int posicoes3 [4] = {10, 0, 50, 60};
+cout << dirMaiorDist(posicoes3) << endl;
+//valor esperado: Tras
+cout << maiorDist(posicoes3) << endl;
+//valor esperado: 60
+// "Direita", "Esquerda", "Frente", "Tras"
+int posicoes4 [4] = {54, 55, 30, 0};
+cout << dirMaiorDist(posicoes4) << endl;
+//valor esperado: Esquerda
+cout << maiorDist(posicoes4) << endl;
+//valor esperado: 55
+}
+*/
 
 // 5 - Faça uma função que pergunta ao usuário se ele deseja continuar o mapeamento e 
 // retorna verdadeiro ou falso9y
@@ -139,15 +206,26 @@ bool mapeamento(){
   cout << "Gostaria de Continuar?"<<endl;
   cin >> verificacao;
 
-  while(verificacao == 1){
-    cout << "O mapeamento irá continuar"<<endl;
-    cout << "Gostaria de Continuar?"<<endl;
-    cin >> verificacao; 
+  if(verificacao == 1){
+    return 1;
   }
-    cout << "O mapeamento será encerrado"<<endl;
-  
-  return 0;
+  else{
+    return 0;
+  }
 }
+
+/* Descomente essa função para poder realizar o teste de uso 
+int main() {
+  int parar = 0;
+  while (parar == 0) {
+    parar = mapeamento();
+  }
+  // A função deve perguntar: "Digite 1 para parar o mapeamento!"
+  // Se o usuário digitar 1, deve sair do loop while declarado acima
+  // Em caso contrário, continua no loop acima e pergunta novamente
+  return 1;
+}
+*/
 
 // 6 - A função abaixo (que está incompleta) vai "dirigindo" virtualmente um robô 
 // e através de 4 sensores em cada um dos 4 pontos do robo ("Direita", "Esquerda", 
